851a31620f989890e17160e4ee816a2add7777de
birajstha:construction_worker::penguin <birajstha@gmail.com>
2024-09-27 13:46:35 -0400

    added gen_spike scripts

--- gen_spike_lenient.py
+++ gen_spike_lenient.py
@@ -7,7 +7,7 @@ from colorama import Fore, Style, init
 # Initialize colorama
 init(autoreset=True)
 
-workdir = "/ocean/projects/med220004p/bshresth/vannucci/all_runs/scripts/outputs/old_outputs/ANTS_FSL_noBBR_strict/working/pipeline_cpac_fmriprep-options/"           
+workdir = "/ocean/projects/med220004p/bshresth/vannucci/all_runs/scripts/outputs/old_outputs/ANTS_FSL_noBBR_lenient/working/pipeline_cpac_fmriprep-options/"           
 
 def run(args):
     sub, ses, scan, reg, file_path = args
@@ -16,10 +16,11 @@ def run(args):
         return
 
     if os.path.exists(fdj):
+        #print(Fore.GREEN + f"Processing sub-{sub}, ses-{ses}, scan-{scan}")
         # Read in the FDJ file
         fdj_df = pd.read_csv(fdj, header=None)
        
-        threshold_df = (fdj_df > 0.2).astype(int)
+        threshold_df = (fdj_df > 0.9).astype(int)
 
         inverted_threshold_df = (1 - threshold_df)
 
@@ -30,18 +31,18 @@ def run(args):
         qc_df['inverted_thresholded_fdj'] = inverted_threshold_df
 
         # Construct new file path
-        new_path = os.path.join(os.getcwd(), 'TSVs_strict')
+        new_path = os.path.join(os.getcwd(), 'TSVs_lenient')
         os.makedirs(new_path, exist_ok=True)
         new_file = f'spikes_sub-{sub}_ses-{ses}_scan-{scan}.tsv'
         new_file_path = os.path.join(new_path, new_file)
         inverted_threshold_df.to_csv(new_file_path, header=None, index=False, sep='\t')
         
         #construct new file path for qc_df
-        new_path_qc = os.path.join(os.getcwd(), 'TSVs_strict', 'qc')
+        new_path_qc = os.path.join(os.getcwd(), 'TSVs_lenient', 'qc')
         new_file_qc = f'qc_sub-{sub}_ses-{ses}_scan-{scan}.tsv'
         new_file_path_qc = os.path.join(new_path_qc, new_file_qc)
         os.makedirs(new_path_qc, exist_ok=True)
-        qc_df.to_csv(new_file_path_qc, header=['FD_J', 'thresholded_at_0.2', 'inverted'], index=False, sep='\t')
+        qc_df.to_csv(new_file_path_qc, header=['FD_J', 'thresholded_at_0.9', 'inverted'], index=False, sep='\t')
 
     else:
         print(Fore.RED + f"NO FDJ FILE for sub-{sub}, ses-{ses}, scan-{scan}")
